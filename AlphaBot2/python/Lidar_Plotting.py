57477a2095e8695e1ddb9cc0ad81cb7d020d8547
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-04-05 10:00:12 -0700

    deleted windows plotting code

--- AlphaBot2/python/Lidar_Plotting.py
+++ AlphaBot2/python/Lidar_Plotting.py
@@ -1,20 +1,4 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from rplidar import RPLidar
-
-# PORT = '/dev/ttyUSB0'
-
-# lidar = RPLidar(PORT)
-
-# def get_scan_data():
-#     for i, scan in enumerate(lidar.iter_scans()):
-#         print(f"Scan {i+1}: {len(scan)} points")
-#     return scan
-
-# print(get_scan_data())
-
 from rplidar import RPLidar
-import time
 
 # Set your Lidar's USB port (Check with `ls /dev/ttyUSB*`)
 PORT_NAME = "/dev/ttyUSB0"  # Default for Raspberry Pi
