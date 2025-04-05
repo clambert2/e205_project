4f7155d20d9b7d893d761a8492fa9c2f9de089fb
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-04-05 11:11:21 -0700

    slowing robot + square path

--- AlphaBot2/python/Square_Data_Run.py
+++ AlphaBot2/python/Square_Data_Run.py
@@ -1,5 +1,34 @@
 import time
 from AlphaBot2 import AlphaBot2
 from rplidar import RPLidar
+from Lidar_Test import run_lidar
 
+if __name__ == "__main__":
+    
+    # Create AlphaBot2 object
+    Ab = AlphaBot2()
 
+    # Set the robot to move a bit slower
+    Ab.PA = 20
+    Ab.PB = 20
+
+    # Create Lidar object
+    lidar = RPLidar("/dev/ttyUSB0", baudrate=460800)
+    lidar.stop()
+    lidar.connect()
+    lidar.start_motor()
+
+    # Create lidar scan list
+    scan_list = []
+
+    for i in range(4):
+        # Get Lidar scan data
+        lidar.append(lidar.iter_scans())
+        Ab.forward()
+        time.sleep(1)
+        Ab.stop()
+        time.sleep(1)
+        Ab.right()
+        time.sleep(1)
+        Ab.stop()
+        time.sleep(1)
