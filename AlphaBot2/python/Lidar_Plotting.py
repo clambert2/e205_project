9a2d827141b9715a7a0613808b6e6b76ccd57c73
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-04-05 10:02:54 -0700

    comented and cleaned up data code

--- AlphaBot2/python/Lidar_Plotting.py
+++ AlphaBot2/python/Lidar_Plotting.py
@@ -1,24 +1,30 @@
 from rplidar import RPLidar
 
-# Set your Lidar's USB port (Check with `ls /dev/ttyUSB*`)
 PORT_NAME = "/dev/ttyUSB0"  # Default for Raspberry Pi
 
 def run_lidar():
     """Reads and prints data from RP Lidar."""
-
+    # Initialize the Lidar
     lidar = RPLidar(PORT_NAME, baudrate=460800)
-    print("✅ Lidar connected! Reading data... (Press Ctrl+C to stop)")
+    print("Lidar connected! Reading data... (Press Ctrl+C to stop)")
+
+    # Start the Lidar and connect
     lidar.stop()
     lidar.connect()
+
+    # Get the health and info of the Lidar
     print(lidar.get_health())
     print(lidar.get_info())
+    # Start the motor and collect a scaning
     lidar.start_motor()
     scan = lidar.iter_scans()
+
+    # Print the scan data
     for s in scan:
         print(s)
         break
 
-
+    # Stop the Lidar and disconnect
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
