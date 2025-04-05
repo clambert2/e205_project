3d66ae388309a538c5f40ac4a2f3ef2f044adcdb
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-04-05 10:36:03 -0700

    added print boolean to lidar test

--- AlphaBot2/python/Lidar_Test.py
+++ AlphaBot2/python/Lidar_Test.py
@@ -2,7 +2,7 @@ from rplidar import RPLidar
 
 PORT_NAME = "/dev/ttyUSB0"  # Default for Raspberry Pi
 
-def run_lidar():
+def run_lidar(print_data=False):
     """Reads and prints data from RP Lidar."""
     # Initialize the Lidar
     lidar = RPLidar(PORT_NAME, baudrate=460800)
@@ -20,14 +20,18 @@ def run_lidar():
     scan = lidar.iter_scans()
 
     # Print the scan data
-    for s in scan:
-        print(s)
-        break
+    if print_data:
+        for s in scan:
+            print(s)
+            break
 
     # Stop the Lidar and disconnect
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
+
+    # Return the scan data
+    return scan
 
 if __name__ == "__main__":
     run_lidar()
---
+++ AlphaBot2/python/Square_Data_Run.py
@@ -0,0 +1,5 @@
+import time
+from AlphaBot2 import AlphaBot2
+from rplidar import RPLidar
+
+
