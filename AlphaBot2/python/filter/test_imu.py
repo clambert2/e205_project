89ebc1257e4a068f7c1ba3e3f58615f1eed7ac57
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-04-23 21:52:40 -0700

    movement and stationary have good enough IMU data

--- AlphaBot2/python/filter/test_imu.py
+++ AlphaBot2/python/filter/test_imu.py
@@ -2,6 +2,8 @@ from imu import IMU
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from AlphaBot2 import AlphaBot2
+
 
 # Write some imu test code that prints to terminal the values of the imu and integrated values
 
@@ -36,4 +38,39 @@ if __name__=='__main__':
         print(f"Integrated Position: x={position[0]:.3f}, y={position[1]:.3f}, theta={position[2]:.3f}")
         print(f"Integrated Velocity: vx={velocity[0]:.3f}, vy={velocity[1]:.3f}, vtheta={position[2]:.3f}")
         print(f"Time passed: {time_passed:.3f} s")
+        print("")
 
+    # Then perform test when the robot performs a movement
+    # Initialize the robot
+    alpha_bot = AlphaBot2()
+    alpha_bot.setPWMA(20)
+    alpha_bot.setPWMB(20)
+    alpha_bot.forward()
+    time.sleep(0.5)
+    alpha_bot.stop()
+
+    samples = imu.get_data()
+
+    position = np.zeros(3)  # [x, y, theta]
+    velocity = np.zeros(3)  # [vx, vy, vtheta]
+    last_timestamp = 0.00
+    time_passed = 0.00
+    for i, sample in enumerate(samples):
+        timestamp, ax, ay, az, gx, gy, gz = sample
+        print(f"Timestamp: {timestamp:.3f} s")
+        print(f"Acceleration: ax={ax:.3f}, ay={ay:.3f}, az={az:.3f}")
+        print(f"Gyroscope: gx={gx:.3f}, gy={gy:.3f}, gz={gz:.3f}")
+        print("")
+
+        dt = (timestamp - timestamp) if  i < 1 else (timestamp - samples[i-1][0])
+        time_passed += dt
+        print(f"Time delta: {dt:.3f} s")
+        velocity[0] += ax * dt
+        velocity[1] += ay * dt
+        position[0] += velocity[0] * dt
+        position[1] += velocity[1] * dt
+        position[2] += gz * dt
+        print(f"Integrated Position: x={position[0]:.3f}, y={position[1]:.3f}, theta={position[2]:.3f}")
+        print(f"Integrated Velocity: vx={velocity[0]:.3f}, vy={velocity[1]:.3f}, vtheta={position[2]:.3f}")
+        print(f"Time passed: {time_passed:.3f} s")
+        print("")
