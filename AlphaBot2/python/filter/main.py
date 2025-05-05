501215ff03fb910fe3a60bc9e550a26afbe35357
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-05-05 01:14:26 -0700

    uncommented movement code

--- AlphaBot2/python/filter/main.py
+++ AlphaBot2/python/filter/main.py
@@ -16,18 +16,18 @@ from rplidar import RPLidar
 if __name__=='__main__':
 
     ab2 = AlphaBot2()
-    # high_speed_thread = PoseThread(interval=0.01)
-    # high_speed_thread.start()
+    high_speed_thread = PoseThread(interval=0.01)
+    high_speed_thread.start()
     time.sleep(1)
-    # print("pose: ", high_speed_thread.get_pose())
+    print("pose: ", high_speed_thread.get_pose())
 
-    # control_to_pose(high_speed_thread, ab2, 0.2, 0.2,
-    #                 kp_lin=5, ki_lin=2,
-    #                 kp_ang=10, ki_ang=2,
-    #                 max_power=30, min_power=10,
-    #                 pos_tolerance=0.06, ang_tolerance=0.2)
+    control_to_pose(high_speed_thread, ab2, 0.2, 0.2,
+                    kp_lin=5, ki_lin=2,
+                    kp_ang=10, ki_ang=2,
+                    max_power=30, min_power=10,
+                    pos_tolerance=0.06, ang_tolerance=0.2)
 
-    # print("pose: ", high_speed_thread.get_pose())
+    print("pose: ", high_speed_thread.get_pose())
 
-    print(get_scan())
-    # high_speed_thread.stop()
+    # print(get_scan())
+    high_speed_thread.stop()
